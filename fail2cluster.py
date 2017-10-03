00dbe0f47014f5c14b8b49af7a141a917a5d41aa
Seth Ryder <ryder.seth@gmail.com>
2017-10-03 16:55:15 -0400

    add aws waf support, switch to dynamodb
    switched to dynamodb for storing bans and information.

--- fail2cluster.py
+++ fail2cluster.py
@@ -2,11 +2,18 @@
 
 #config
 default_ban_timeout = 86400
-redis_hostname = ''
-redis_port = 6379
-nginx_reload_command = "/etc/init.d/nginx reload"
+aws_access_key = ""
+aws_secret_key = ""
+dynamodb_ban_table = "fail2ban_banned_ips"
+dynamodb_whitelist_table = "fail2ban_whitelist_ips"
+dynamodb_auditlog_table = "fail2ban_audit_log"
+#nginx settings
+nginx_enabled = False
+nginx_reload_command = "systemctl reload nginx"
 nginx_deny_file = "/etc/nginx/deny.conf"
-waf_enabled = False
+#aws waf settings
+waf_enabled = True
+waf_ip_set = ""
 
 import boto3
 import botocore
@@ -14,11 +21,13 @@ import datetime
 import hashlib
 import getopt
 import json
+import operator
 import os
-import redis
 import socket
 import sys
 import time
+import uuid
+from boto3.dynamodb.conditions import Key, Attr
 from subprocess import Popen, PIPE
 from string import Template
 
@@ -32,123 +41,273 @@ def usage(opt = 'all'):
     else:
         print 'Usage: fail2cluster.py (option)'
         print ''
-        print '-b, --ban <ip> [<seconds>]               Ban an IP. Optional: How many seconds until the IP expires, also accepts "never".'
+        print '-b, --ban <ip> [<seconds> "<note>"]       Ban an IP. Optional: How many seconds until the IP expires, also accepts "never". Can also include a note.'
         print '-u, --unban <ip>                         Unban an IP.'
-        print '-w, --whitelist <ip>                     Whitelist an IP. Will also unban it if it\'s banned.'
+        print '-w, --whitelist <ip> ["<note>"]          Whitelist an IP. Will also unban the IP if it\'s banned.'
         print '-r, --unwhitelist <ip>                   Remove an IP from the whitelist.'
         print '-l, --list-bans                          List all banned IPs and when they expire.'
+        print '-l, --list-whitelisted-ips               List all whitelisted IPs and any included notes.'
         print '-n, --nginx-format-list                  Write bans to a specified file that can be loaded by nginx.'
         print '-h, --help                               Prints out this usage. You are looking at it now!'
         print ''
 
-def redis_init():
-    try:
-        r = redis.StrictRedis(host=redis_hostname, port=redis_port, db=0, socket_timeout=2)
-    except redis.ConnectionError:
-        sys.exit()
-    return r
-
-def ban_ip(ip, expires):
+def ban_ip(ip, expires, note):
+    now = int(time.time())
     validate_ip(ip)
-    r = redis_init()
-    exists = r.zrank('banned_ips', ip)
-    whitelisted = r.sismember('whitelisted_ips', ip)
+    dynamodb = init_aws_dynamodb(dynamodb_ban_table)
 
     if expires == 'never':
-        ttl = 2147483647
+        ttl = 'never'
     else:
-        ttl = int(time.time()) + expires
+        ttl = int(time.time()) + int(expires)
 
-    if exists is not None:
+    if check_ip_exists(ip):
         print ip + " is already banned."
         return False
 
-    if whitelisted:
+    if check_ip_whitelisted(ip):
         print ip + " is whitelisted, not banning."
         return False
 
     if waf_enabled:
         waf_ban_ip(ip)
 
-    r.zadd('banned_ips', ttl, ip)
+    dynamodb.put_item(
+       Item={
+            'ip': ip,
+            'created': now,
+            'expires': ttl,
+            'note': note
+        }
+    )
+
+    if nginx_enabled:
+        write_nginx_deny_list(nginx_deny_file)
+
+    audit_log(ip, 'add', now, ttl, note)
     print "Added " + ip + " to ban list."
     return True
 
-def unban_ip(ip):
+def unban_ip(ip, note=' '):
     validate_ip(ip)
-    r = redis_init()
-    exists = r.zrank('banned_ips', ip)
+    dynamodb = init_aws_dynamodb(dynamodb_ban_table)
 
-    if exists is None:
+    if not check_ip_exists(ip):
         print ip + " is not banned."
         return False
 
     if waf_enabled:
         waf_unban_ip(ip)
 
-    r.zrem('banned_ips', ip)
+    dynamodb.delete_item(Key={'ip': ip})
+
+    if nginx_enabled:
+        write_nginx_deny_list(nginx_deny_file)
+
+    audit_log(ip, 'remove', note="expired")
     print ip + " has been removed."
     return True
 
-def whitelist_ip(ip):
+def whitelist_ip(ip, note):
     validate_ip(ip)
-    r = redis_init()
-    exists = r.sismember('whitelisted_ips', ip)
-    banned = r.zrank('banned_ips', ip)
+    now = int(time.time())
+    dynamodb = init_aws_dynamodb(dynamodb_whitelist_table)
 
-    if exists:
+    if check_ip_whitelisted(ip):
         print ip + " is already whitelisted."
         return False
 
-    if banned is not None:
+    if check_ip_exists(ip):
         print ip + " is banned, removing from ban list first."
         unban_ip(ip)
 
-    r.sadd('whitelisted_ips', ip)
+    dynamodb.put_item(
+       Item={
+            'ip': ip,
+            'created': now,
+            'note': note,
+        }
+    )
+
     print "Added " + ip + " to whitelist."
     return True
 
 def unwhitelist_ip(ip):
     validate_ip(ip)
-    r = redis_init()
-    exists = r.sismember('whitelisted_ips', ip)
+    dynamodb = init_aws_dynamodb(dynamodb_whitelist_table)
 
-    if not exists:
+    if not check_ip_whitelisted(ip):
         print ip + " is not whitelisted."
         return False
 
-    r.srem('whitelisted_ips', ip)
+    dynamodb.delete_item(Key={'ip': ip})
+
     print "Removed " + ip + " from whitelist."
     return True
 
+def check_ip_exists(ip):
+    table = init_aws_dynamodb(dynamodb_ban_table)
+    response = table.query(
+        KeyConditionExpression=Key('ip').eq(ip)
+    )
+    if response['Count'] > 0:
+        return True
+    else:
+        return False
+
+def check_ip_whitelisted(ip):
+    table = init_aws_dynamodb(dynamodb_whitelist_table)
+    response = table.query(
+        KeyConditionExpression=Key('ip').eq(ip)
+    )
+    if response['Count'] > 0:
+        return True
+    else:
+        return False
+
+def init_aws_waf():
+    session = boto3.Session(
+        aws_access_key_id=aws_access_key,
+        aws_secret_access_key=aws_secret_key
+    )
+    waf = session.client('waf')
+    return waf
+
+def init_aws_dynamodb(table):
+    dynamodb = boto3.resource('dynamodb',
+        region_name='us-east-1',
+        aws_access_key_id=aws_access_key,
+        aws_secret_access_key=aws_secret_key
+    )
+    table = dynamodb.Table(table)
+    return table
+
 def waf_ban_ip(ip):
-    print "waf ban ip"
+    validate_ip(ip)
+    client = init_aws_waf()
+    change_token = client.get_change_token()
+    try:
+        response = client.update_ip_set(
+            IPSetId=waf_ip_set,
+            ChangeToken=change_token['ChangeToken'],
+            Updates=[
+                {
+                    'Action': 'INSERT',
+                    'IPSetDescriptor': {
+                        'Type': 'IPV4',
+                        'Value': ip + '/32'
+                    }
+                },
+            ]
+        )
+    except botocore.exceptions.ClientError as e:
+        print("Unable to ban IP, error: " + str(e))
 
 def waf_unban_ip(ip):
-    print "waf unban ip"
+    validate_ip(ip)
+    client = init_aws_waf()
+    change_token = client.get_change_token()
+    try:
+        response = client.update_ip_set(
+            IPSetId=waf_ip_set,
+            ChangeToken=change_token['ChangeToken'],
+            Updates=[
+                {
+                    'Action': 'DELETE',
+                    'IPSetDescriptor': {
+                        'Type': 'IPV4',
+                        'Value': ip + '/32'
+                    }
+                },
+            ]
+        )
+    except botocore.exceptions.ClientError as e:
+        print("Unable to ban IP, error: " + str(e))
+
 
 def remove_expired_ips():
-    r = redis_init()
     now = int(time.time())
-    r.zremrangebyscore('banned_ips', 0, now)
 
-def list_ips():
-    r = redis_init()
-    banned_ips = r.zrange('banned_ips', 0, -1)
+    table = init_aws_dynamodb(dynamodb_ban_table)
+    response = table.scan(
+        FilterExpression=Attr('expires').lt(now)
+    )
+    expired_ips = response['Items']
 
-    total = len(banned_ips)
-    print str(total) + " banned IP(s)"
+    for ip in expired_ips:
+        unban_ip(ip['ip'], 'expired')
+
+def list_banned_ips():
+    dynamodb = init_aws_dynamodb(dynamodb_ban_table)
+
+    response = dynamodb.scan()
+    banned_ips = response['Items']
+
+    total = response['Count']
+    print str(total) + " banned IP(s):"
 
     for ip in banned_ips:
-        print ip
+        ip_len = len(ip['ip'])
+        friendly_ip = ip['ip']
+        if ip_len is not 15:
+            required_spaces = 15 - ip_len
+            friendly_ip += ' ' * required_spaces
+        if ip['expires'] != 'never':
+            expires = datetime.datetime.fromtimestamp(ip['expires'])
+            friendly_date = expires.strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            friendly_date = 'Never'
+        print friendly_ip + " (Expires: " + friendly_date + ")"
+
+def list_whitelisted_ips():
+    dynamodb = init_aws_dynamodb(dynamodb_whitelist_table)
+
+    response = dynamodb.scan()
+    whitelisted = response['Items']
+
+    total = response['Count']
+    print str(total) + " whitelisted IP(s):"
+
+    for ip in whitelisted:
+        ip_len = len(ip['ip'])
+        friendly_ip = ip['ip']
+        if ip_len is not 15:
+            required_spaces = 15 - ip_len
+            friendly_ip += ' ' * required_spaces
+        if ip['note'] != ' ':
+            friendly_note = ip['note']
+        else:
+            friendly_note = 'None'
+        print friendly_ip + " (Note: " + friendly_note + ")"
+
+def audit_log(ip, action, created=' ', expires=' ', note=' '):
+    dynamodb = init_aws_dynamodb(dynamodb_auditlog_table)
+    uid = uuid.uuid4()
+    action_time = int(time.time())
+
+    if not created:
+        created = int(time.time())
+
+    dynamodb.put_item(
+       Item={
+            'ip': ip,
+            'action': action,
+            'action_time': action_time,
+            'created': created,
+            'expires': expires,
+            'note': note
+        }
+    )
 
 def get_nginx_deny_list():
-    r = redis_init()
-    ips = r.zrange('banned_ips', 0, -1)
+    dynamodb = init_aws_dynamodb(dynamodb_ban_table)
     denied_ips = ""
 
-    for ip in ips:
-        denied_ips += "deny " + ip + ";\n"
+    response = dynamodb.scan()
+    banned_ips = response['Items']
+
+    for ip in banned_ips:
+        denied_ips += "deny " + ip['ip'] + ";\n"
 
     return denied_ips
 
@@ -206,12 +365,13 @@ def validate_ip(ip):
 def run():
     ran = False
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "bulhnrw", [
+        opts, args = getopt.getopt(sys.argv[1:], "bulhnrwt", [
           "ban",
           "unban",
           "whitelist",
           "unwhitelist",
           "list-bans",
+          "list-whitelisted-ips"
           "nginx-format-list",
           "help"])
     except getopt.GetoptError, err:
@@ -221,10 +381,13 @@ def run():
     for opt, arg in opts:
         ran = True
         if opt in ("-b", "--ban"):
-            if len(args) < 1 or len(args) > 2:
+            if len(args) < 1 or len(args) > 3:
                 usage('ban')
-            ban_ip(args[0], default_ban_timeout)
-            write_nginx_deny_list(nginx_deny_file)
+            expires = default_ban_timeout
+            note = ' '
+            if len(args) >= 2: expires = args[1]
+            if len(args) == 3: note = args[2]
+            ban_ip(args[0], expires, note)
             remove_expired_ips()
         elif opt in ("-u", "--unban"):
             if len(args) != 1:
@@ -232,9 +395,11 @@ def run():
             unban_ip(args[0])
             remove_expired_ips()
         elif opt in ("-w", "--whitelist"):
-            if len(args) != 1:
+            if len(args) < 1 or len(args) > 2:
                 usage('whitelist')
-            whitelist_ip(args[0])
+            note = ''
+            if len(args) == 2: note = args[1]
+            whitelist_ip(args[0], note)
             remove_expired_ips()
         elif opt in ("-r", "--unwhitelist"):
             if len(args) != 1:
@@ -242,13 +407,17 @@ def run():
             unwhitelist_ip(args[0])
             remove_expired_ips()
         elif opt in ("-l", "--list-bans"):
-            list_ips()
+            list_banned_ips()
+            remove_expired_ips()
+        elif opt in ("-t", "--list-whitelisted-ips"):
+            list_whitelisted_ips()
             remove_expired_ips()
         elif opt in ("-n", "--nginx-format-list"):
             write_nginx_deny_list(nginx_deny_file)
             remove_expired_ips()
         elif opt in ("-h", "--help"):
             usage()
+            remove_expired_ips()
         else:
            assert False, "unhandled option"
 
@@ -256,7 +425,4 @@ def run():
         usage()
 
 if __name__ == "__main__":
-    try:
-        run()
-    except redis.ConnectionError:
-        sys.exit()
+    run()
